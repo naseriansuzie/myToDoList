30c9995e5a9b579e60315f83713424e03de88c60
Sooji Kim <yh0gogo@gmail.com>
2019-12-16 16:41:34 +0900

    Updates

---
+++ precache-manifest.45cc70c50988a23a09c90ca2bb799220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4839dd62bdf1b23746b0ea2859d869",
+    "url": "/myToDoList/index.html"
+  },
+  {
+    "revision": "0c4c06a8b25a00d57724",
+    "url": "/myToDoList/static/css/main.f90ab08f.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/myToDoList/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myToDoList/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c4c06a8b25a00d57724",
+    "url": "/myToDoList/static/js/main.f245236c.chunk.js"
+  },
+  {
+    "revision": "6bc542f5711535fc31d8",
+    "url": "/myToDoList/static/js/runtime-main.6ba0a77d.js"
+  }
+]);
